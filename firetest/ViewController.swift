5c7dd1639c38cf0980c911c35fd9dcb6192596f5
raynashah02 <raynaemail@gmail.com>
2022-12-19 22:27:07 -0600

    Initial Commit

---
+++ firetest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firetest
+//
+//  Created by Rayna Shah on 12/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
